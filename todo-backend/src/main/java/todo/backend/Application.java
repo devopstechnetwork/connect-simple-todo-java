561409cd97ada820734dd2c473216997f19bcaab
Richard Vowles <richard@bluetrainsoftware.com>
2019-05-10 22:01:31 +1200

    Sorry - there is a bit of overlap here as I needed to test if its working, and this is testing. You may wish to rewrite some of this, typically the FeatureInitializer would go in your todo-features but you'll need to include some more deps (such as composite-jersey from the todo project).

--- todo-backend/src/main/java/todo/backend/Application.java
+++ todo-backend/src/main/java/todo/backend/Application.java
@@ -3,17 +3,12 @@ package todo.backend;
 import cd.connect.jersey.common.CommonConfiguration;
 import cd.connect.jersey.common.InfrastructureConfiguration;
 import cd.connect.jersey.common.LoggingConfiguration;
+import cd.connect.jersey.common.TracingConfiguration;
 import cd.connect.lifecycle.ApplicationLifecycleManager;
 import cd.connect.lifecycle.LifecycleStatus;
-import cd.connect.opentracing.LoggingSpanTracer;
-import io.jaegertracing.Configuration;
-import io.opentracing.Tracer;
 import io.opentracing.contrib.jaxrs2.client.ClientTracingFeature;
-import io.opentracing.contrib.jaxrs2.server.ServerTracingDynamicFeature;
-import io.opentracing.util.GlobalTracer;
 import io.prometheus.client.hotspot.DefaultExports;
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
@@ -36,8 +31,7 @@ public class Application {
 		// turn on all jvm prometheus metrics
     DefaultExports.initialize();
 
-    // register the open-tracing tracer
-    GlobalTracer.register(new LoggingSpanTracer(getTracer()));
+    FeatureInitializer.initialize();
 
     // register our resources, try and tag them as singleton as they are instantiated faster
 		ResourceConfig config = new ResourceConfig(
@@ -45,15 +39,8 @@ public class Application {
       ClientTracingFeature.class,
       CommonConfiguration.class,
       LoggingConfiguration.class,
+      TracingConfiguration.class,
       InfrastructureConfiguration.class);
-
-		config.register(new AbstractBinder() {
-			@Override
-			protected void configure() {
-				// this is awkward but required, otherwise it simply doesn't work.
-				bind(new ServerTracingDynamicFeature.Builder(GlobalTracer.get()).withJoinExistingActiveSpan(false).build());
-			}
-		});
 
     final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, config, true);
 
@@ -68,11 +55,5 @@ public class Application {
 		Thread.currentThread().join();
 	}
 
-	private static Tracer getTracer() {
-    return  new io.jaegertracing.Configuration(System.getProperty("app.name", "local-app"))
-      // We need to get a builder so that we can directly inject the
-      // reporter instance.
-      .withReporter(Configuration.ReporterConfiguration.fromEnv())
-      .withSampler(Configuration.SamplerConfiguration.fromEnv().withType("const").withParam(1)).getTracer();
-  }
+
 }
