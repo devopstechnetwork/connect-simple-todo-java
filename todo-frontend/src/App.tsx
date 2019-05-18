2fa16f8df24c48e0093b781ec9b04e0d7b320e2a
Irina Southwell <irinasouthwell@gmail.com>
2019-05-19 11:34:11 +1200

    Frontend (#6)
    * extract and generate a typescript api.
    
    * make the front-end app work
    
    * update readme

--- todo-frontend/src/App.tsx
+++ todo-frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
-import TodoApi, { Todo } from './generated-api';
+import { Todo, DefaultApi } from './api';
 import './App.css';
+import { Configuration } from './api';
 
-const todoApi = new TodoApi('http://simple-app-api.connect.cd:3001');
+const todoApi = new DefaultApi(new Configuration({basePath: 'http://localhost:8099'}));
 
 class App extends React.Component<{}, { todos: Array<Todo> }> {
   private titleInput: HTMLInputElement;
 
   constructor() {
-    super();
+    super([]);
 
     this.state = {
       todos: [],
@@ -16,7 +17,7 @@ class App extends React.Component<{}, { todos: Array<Todo> }> {
   }
 
   async componentDidMount() {
-    const todos = await todoApi.getTodos({});
+    const todos = (await todoApi.listTodos({})).data;
     this.setState({ todos });
   }
 
@@ -27,17 +28,17 @@ class App extends React.Component<{}, { todos: Array<Todo> }> {
       resolved: false,
     };
 
-    const todos = await todoApi.addTodo({ body: todo });
+    const todos = (await todoApi.addTodo(todo )).data;
     this.setState({ todos });
   }
 
   async removeToDo(id: string) {
-    const todos = await todoApi.removeTodo({ id });
+    const todos = (await todoApi.removeTodo(id)).data;
     this.setState({ todos });
   }
 
   async doneToDo(id: string) {
-    const todos = await todoApi.resolveTodo({ id });
+    const todos = (await todoApi.resolveTodo(id)).data;
     this.setState({ todos });
   }
 
@@ -64,16 +65,17 @@ class App extends React.Component<{}, { todos: Array<Todo> }> {
         <ul>
           {this.state.todos.map((todo, index) => {
             return (
-              <li className='qa-main'
+              <li
+                className="qa-main"
                 key={index}
                 style={{
                   textDecoration: todo.resolved ? 'line-through' : 'none',
                 }}
               >
                 {!todo.resolved && (
-                  <button onClick={() => this.doneToDo(todo.id)} className='qa-done-button'>Done</button>
+                  <button onClick={() => this.doneToDo(todo.id || '')} className="qa-done-button">Done</button>
                 )}
-                <button onClick={() => this.removeToDo(todo.id)} className='qa-delete-button'>
+                <button onClick={() => this.removeToDo(todo.id || '')} className="qa-delete-button">
                   Delete
                 </button>{' '}
                 {todo.title}
